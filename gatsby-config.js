e61e60eb0cbd7e53c7e34e388a2098f70dfed0b1
Giovanni Bieller <giovanni.bieller@gmail.com>
2020-04-30 11:48:28 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Gatsby + Netlify POC`,
+    author: `Giovanni Bieller`,
+    description: `A POC site/blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `giovannibieller`,
     },
   },
   plugins: [
